046ef15da170c0d1b03f5556cd54e897060fd7f4
Andrezzzz421 <andresdavidproxd@gmail.com>
2024-01-28 00:02:16 -0500

    trabajo

---
+++ dia2/Ejercicio1_PerezAndres.py
@@ -0,0 +1,34 @@
+def fibonacci(n):
+    fibonacci_seq = [0, 1]
+    while len(fibonacci_seq) < n:
+        fibonacci_seq.append(fibonacci_seq[-1] + fibonacci_seq[-2])
+    return fibonacci_seq
+
+def main():
+    print("La secuencia de Fibonacci es aquella en la cual se suma los dos numeros anteriores para obtener el siguiente, empezando desde 0 y 1.")
+
+    while True:
+        try:
+            n = int(input("Ingrese el valor de 'n' para generar la secuencia de Fibonacci: "))
+         
+
+            if n == 0:
+                print("¡Hasta luego! Gracias por usar el Generador de Fibonacci.")
+                break
+
+            if n < 0:
+                print("Por favor, ingrese un valor no negativo.")
+                continue
+
+            result = fibonacci(n)
+            print(f"Secuencia de Fibonacci hasta el termino {n}: {result}")
+
+            continuar = input("¿Desea continuar? (Sí/No): ").lower()
+            if continuar != 'sí' and continuar != 'si':
+                print("¡Hasta luego! Gracias por usar el generador de fibonacci.")
+                break
+        except ValueError:
+            print("Por favor, ingrese un valor entero.")
+
+if __name__ == "__main__":
+    main()
