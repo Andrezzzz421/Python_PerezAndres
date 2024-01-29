c899f26a5c664ec98c9a6f3d4b9bffb2000debfd
Andrezzzz421 <andresdavidproxd@gmail.com>
2024-01-28 19:03:40 -0500

    trabajo

---
+++ dia2/Ejercicio2_PerezAndres.py
@@ -0,0 +1,26 @@
+import random
+
+def juego_alzar():
+    numero_aleatorio = random.randint(1, 100)
+    print("Bienvenido querido usuario al juego del alzar, Usted tendra que adivinar el numero secreto que esta entre el 1 y el 100")
+    print("¡Cabe recalcar que solamente tiene 10 intentos mucha suerte usuario!")
+    intentos=0
+    while intentos<10:
+        try:
+            numero_usuario = int(input("Ingrese un numero: "))
+            if numero_usuario == numero_aleatorio:
+                print(f"FELICIDADES,Has acertado el numero {numero_aleatorio}")
+                break
+            elif numero_usuario < numero_aleatorio:
+                print(f"El numero ingresado es menor que el numero escojido al azar ")
+            elif numero_usuario > numero_aleatorio:
+                print(f"El numero ingresado es mayor que el numero escojido al azar")
+            intentos+=1
+        except ValueError:
+            print("Por favor, ingrese un valor entero.")
+    if intentos == 10:
+        print(f"Lo siento pero se le acabarons sus intentos. el numero secreto era {numero_aleatorio}")
+
+if __name__ == "__main__":
+    juego_alzar()
+    
