daafa784c62f35ec51518ec1c5e31dea1c183518
Andrezzzz421 <andresdavidproxd@gmail.com>
2024-02-01 19:25:08 -0500

    trabajo

--- dia6/ejercicio6_1_PerezAndres.py
+++ dia6/ejercicio6_1_PerezAndres.py
@@ -2,7 +2,8 @@ def negate(num):
     return -num
 def large_num(num):
     res = (num>10000)
-b=10001
+    return res
+b=int(input(""))
 negate(b)
 neg_b=negate(b)
 print('b:',b,'neg_b:', neg_b)
@@ -12,4 +13,4 @@ print('b is big:',big)
 
 #1-Los valores que se guardan para que se impriman no estan entre parentesis
 #2-" B "no estaba definida
-#3-
+#3-En la funcion large_num faltaba colocar un return para que no s devolviera que si num era mayor a 10000 seria true si no seria false
---
+++ dia6/ejercicio6_2._PerezAndres.py
@@ -0,0 +1,9 @@
+
+##Mutable: Puede cambiar después de crearse.
+
+##Listas
+##Diccionarios
+##Inmutable: No puede cambiar después de crearse.
+
+##Cadenas
+##Tuplas
