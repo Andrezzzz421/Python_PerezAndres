fa08dfb9563a4c080fb27e17e668b7367576a2bf
Andrezzzz421 <156430410+Andrezzzz421@users.noreply.github.com>
2024-02-08 07:49:53 -0500

    Update ejercicio1.py

--- dia8/ejercicio1.py
+++ dia8/ejercicio1.py
@@ -2,11 +2,11 @@ import json
 
 mijson = open('data.json')
 variable = json.load(mijson)
-mijson.close()
 
 def fecha(pedido):
     return pedido['fecha']
 
 pedidos = variable['ventas']['pedidos']
 pedidos_ordenados = sorted(pedidos, key=fecha, reverse=True)
-print(pedidos_ordenados)
+for i in pedidos_ordenados:
+    print(i)
